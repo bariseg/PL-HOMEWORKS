1602822f1d0aeb0bc13fd755dbe1abd31d405bfb
bariseg <93949921+bariseg@users.noreply.github.com>
2024-11-16 18:56:58 +0300

    damn son alles gut

--- hw2/210104004041/gpp_lexer.lisp
+++ hw2/210104004041/gpp_lexer.lisp
@@ -12,23 +12,61 @@
 
 ;;Classify a token as keyword, operator, literal, identifier, or syntax error.
 (defun classify-token (token)
-    (let ((token-str (string token)))
+    (let ((token-str (string token))) ;; Convert token to string for comparison
         (cond
-            ((member token-str  *gpp-keywords*) (intern (format nil "KW_~A" (string-upcase token-str ))))
-            ((member token-str  *gpp-operators*) (intern (format nil "OP_~A" (string-upcase token-str ))))
-            ((find #\: token-str ) 'VALUEF) ;; Fractions contain ':'
-            ((every #'digit-char-p token-str ) 'VALUEI) ;; Integers
+            ;; Check if the token is a keyword
+            (   
+                (member token-str *gpp-keywords* :test #'string=)
+                (intern (format nil "KW_~A" (string-upcase token-str)))
+            )
+        
+            ;; Check if the token is an operator
+            ((string= token-str "+") 'OP_PLUS)
+            ((string= token-str "-") 'OP_MINUS)
+            ((string= token-str "*") 'OP_MULT)
+            ((string= token-str "/") 'OP_DIV)
+            ((string= token-str "(") 'OP_OP)
+            ((string= token-str ")") 'OP_CP)
+            ((string= token-str ",") 'OP_COMMA)
+
+            ;; Check for fractions (e.g., 123:45)
             (
-                (and    (alpha-char-p (aref token-str 0))
-                        (every (lambda (c) (or (alpha-char-p c) (digit-char-p c) (char= c #\_))) token-str )
+                (and (find #\: token-str)
+                    (
+                        let* (
+                            (colon-pos (position #\: token-str))
+                            (num1 (subseq token-str 0 colon-pos))
+                            (num2 (subseq token-str (1+ colon-pos)))
+                        )
+                        (and 
+                            (not (string= num1 "")) ;; Ensure num1 is not empty
+                            (not (string= num2 "")) ;; Ensure num2 is not empty
+                            (every #'digit-char-p num1)
+                            (every #'digit-char-p num2)
+                        )
+                    )
+                )
+                'VALUEF
+            )
+            
+            ;; Check for integers (e.g., 123)
+            ((every #'digit-char-p token-str) 'VALUEI)
+            
+            ;; Check for valid identifiers (e.g., my_var123)
+            (
+                (and (alpha-char-p (aref token-str 0))
+                    (every (lambda (c) (or (alpha-char-p c) (digit-char-p c) (char= c #\_))) token-str)
                 )
                 'IDENTIFIER
-            ) ;; Valid identifiers
+            )
             
+            ;; Otherwise, it's a syntax error
             (t 'SYNTAX_ERROR)
-        ) ;; Syntax error
+        )
     )
 )
+
+
 
 (defun tokenize (line)
 
@@ -36,42 +74,41 @@
         ;;initialized tokens and current-token
         let ((tokens '()) (current-token ""))
 
-        (loop for char across line do
-            (cond
-                ;; Ignore whitespace
-                (
-                    (member char '(#\Space #\Tab #\Newline #\Return))
+        (if (and (>= (length line) 2) (string= (subseq line 0 2) ";;"))
+            (push 'COMMENT tokens)
+
+            ;;if not comment, process the line for tokens
+            (loop for char across line do
+                (cond
+                    ;; Ignore whitespace
                     (
-                        when (not (string= current-token ""))
-                        (push (classify-token current-token) tokens)
-                        (setf current-token "")
+                        (member char '(#\Space #\Tab #\Newline #\Return))
+                        (
+                            when (not (string= current-token ""))
+                            (push (classify-token current-token) tokens)
+                            (setf current-token "")
+                        )
                     )
-                )
 
-                ;; Comments (start with ;;)
-                (
-                    (and (char= char #\;) (not (string= current-token "")))
-                    (push 'COMMENT tokens)
-                    (return)
-                )
-
-                ;; Operators
-                (
-                    (member (string char) *gpp-operators*)
-                    (when (not (string= current-token ""))
-                        (push (classify-token current-token) tokens)
-                        (setf current-token "")
+                    ;; Operators
+                    (
+                        (member (string char) *gpp-operators* :test #'string=)
+                        (when (not (string= current-token ""))
+                            (push (classify-token current-token) tokens)
+                            (setf current-token "")
+                        )
+                        (push (classify-token (string char)) tokens)
                     )
-                    (push (classify-token (string char)) tokens)
-                )
 
-                ;; Collect characters for tokens
-                (t (setf current-token (concatenate 'string current-token (string char))))
+                    ;; if the string is now a token yet, we collect them
+                    ;; Collect characters for tokens
+                    (t (setf current-token (concatenate 'string current-token (string char))))
+                )
             )
-            
         )
-
         ;; Push the last token if any
+        ;; for example, if the line ends with a identifier "baris" we need to push it
+        ;;because (t (setf current-token (concatenate 'string current-token (string char)))) part didnt push it
         (when (not (string= current-token ""))
             (push (classify-token current-token) tokens)
         )
@@ -79,6 +116,7 @@
         (reverse tokens)
     )
 )
+
 
 ;;(load "gpp_lexer.lisp")
 ;;(gppinterpreter)
